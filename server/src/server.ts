03244dda04e882dd69465ab9a6a1d2f62ed20161
aniket-tote <anikettote2001@gmail.com>
2024-02-16 00:46:28 +0530

    added port

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
 import app from "./app";
 
-const PORT = process.env.EXPRESS_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 AppDataSource.initialize()
   .then(async () => {
