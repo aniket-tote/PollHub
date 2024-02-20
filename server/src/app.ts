293f932941106a184684e6d40df734391358fef7
aniket-tote <anikettote2001@gmail.com>
2024-02-20 23:53:14 +0530

    added frontend url in cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,12 @@ import * as cookieParser from "cookie-parser";
 const app = express();
 
 app.use(json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 // app.use(authMiddleware); // Apply authentication middleware to all routes
 
